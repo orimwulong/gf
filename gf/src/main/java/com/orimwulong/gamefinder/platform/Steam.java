f51c5dadd1be75637b1609284200a2237bfb20bf
orimwulong <64308029+orimwulong@users.noreply.github.com>
2022-06-16 21:31:32 +1000

    Quick cleaning up of unused imports

--- gf/src/main/java/com/orimwulong/gamefinder/platform/Steam.java
+++ gf/src/main/java/com/orimwulong/gamefinder/platform/Steam.java
@@ -2,11 +2,7 @@ package com.orimwulong.gamefinder.platform;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-
-import javax.naming.OperationNotSupportedException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
